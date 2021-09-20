6ee909aec55e0ab5f9eed8b3e544e5c32da68d3a
EvansVan <evansndiritu@gmail.com>
2021-09-21 01:27:39 +0300

    feat:loop to create buttons

--- src/App.js
+++ src/App.js
@@ -1,68 +1,31 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-
+  
   class App extends Component {
+    state = {
+      categories:['animal', 'career', 'celebrity', 'dev', 'explicit', 'history', 'food', 'political', 'fashion', 'religion', 'music', 'money', 'movie', 'travel', 'science', 'sport'],
+      
+  };
+  
+
     render() {
       return (
-        <div class="container pt-5">
-          <div class= "row">
-            <div class= "col-3">
-              <button id="animal" class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>Animal</button>
-            </div> 
-            <div class= "col-3">
-              <button id="career" class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>Career</button>
-            </div> 
-            <div class= "col-3">
-              <button id="celebrity" class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>Celebrity</button>
-            </div> 
-            <div class= "col-3">
-              <button id="dev" class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>Dev</button>
-            </div> 
-          </div>
-          <div class= "row pt-5">
-            <div class= "col-3">
-              <button id="explicit" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Explicit</button>
-            </div> 
-            <div class= "col-3">
-              <button id="history" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>History</button>
-            </div> 
-            <div class= "col-3">
-              <button id="food" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Food</button>
-            </div> 
-            <div class= "col-3">
-              <button id="political" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Political</button>
-            </div>
-          </div>
-          <div class= "row pt-5">
-            <div class= "col-3">
-              <button id="fashion" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Fashion</button>
-            </div> 
-            <div class= "col-3">
-              <button id="religion" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Religion</button>
-            </div> 
-            <div class= "col-3">
-              <button id="music" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Music</button>
-            </div> 
-            <div class= "col-3">
-              <button id="money" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Money</button>
-            </div>
-          </div>
-          <div class= "row pt-5">
-            <div class= "col-3">
-              <button id="movie" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Movie</button>
-            </div> 
-            <div class= "col-3">
-              <button id="travel" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Travel</button>
-            </div> 
-            <div class= "col-3">
-              <button id="science" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Science</button>
-            </div> 
-            <div class= "col-3">
-              <button id="sport" class="btn btn-primary" type="submit" onClick={this.getChucked.bind(this)}>Sport</button>
-            </div>
-          </div>
-        </div>
+
+        <> 
+        {this.jaza()}         
+        </>
       );
+    }
+    jaza = (e) =>{
+    let jokes = []
+    let sasa = this.state.categories.length;
+    for (let i=0; i<sasa;i++){
+        jokes.push(  
+        <div class= "col-3">
+          <button id={this.state.categories[i]} class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>{this.state.categories[i]}</button>
+        </div>
+      )}
+      return jokes
     }
     getChucked = (e) =>{
       let category = e.target.getAttribute("id");
@@ -74,6 +37,10 @@ import 'bootstrap/dist/css/bootstrap.css';
           })
           .catch(console.log)
     }
-  } 
+    } 
   
 export default App;
+
+
+
+  
