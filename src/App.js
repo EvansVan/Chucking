a1f70c31e1a76b85d0e9cb4eea2e3eaa944d014d
EvansVan <evansndiritu@gmail.com>
2022-01-12 13:42:39 +0300

    feat:cards to display jokes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-  
+
   class App extends Component {
     state = {
-      categories:['animal',
-                 'career',
-                 'celebrity',
-                  'dev',
-                  'explicit', 
-                   'history', 
-                   'food', 
-                   'political',
-                    'fashion', 
-                    'religion', 
-                    'music', 
-                    'money', 
-                    'movie', 
-                    'travel', 
-                    'science', 
-                    'sport']
+      jokes:[]
   };
+    categories= ['animal',
+      'career',
+      'celebrity',
+      'dev',
+      'explicit',
+      'history',
+      'food',
+      'political',
+      'fashion',
+      'religion',
+      'music',
+      'money',
+      'movie',
+      'travel',
+      'science',
+      'sport']
+
     render() {
       return (
         <> 
-        {this.jaza()}         
+        {this.jaza()} 
+        {this.cards()} 
         </>
       );
     }
-    jaza = (e) =>{
-    let jokes = []
-    let sasa = this.state.categories.length;
-    for (let i=0; i<sasa;i++){
-        jokes.push(  
-        <container fluid>
-          <button id={this.state.categories[i]} class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>{this.state.categories[i]}</button>
-        </container>
-      )}
+    jaza = (e) =>{ 
+      let jokes = []
+      let sasa = this.categories.length;
+      for (let i=0; i<sasa;i++){
+          jokes.push(  
+          <container fluid>
+            <button id={this.categories[i]} class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>{this.categories[i]}</button>
+          </container>
+        )}
       return jokes
+    }
+    cards = (e) => {
+      let  cadi = []
+      cadi.push(
+        <div>
+          <center><h1>Jokes</h1></center>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{this.state.jokes.categories}</h5>
+                <p class="card-text">{this.state.jokes.value}</p>
+              </div>
+            </div>
+        </div>
+        )
+    return cadi
     }
     getChucked = (e) =>{
       let category = e.target.getAttribute("id");
@@ -44,7 +62,7 @@ import 'bootstrap/dist/css/bootstrap.css';
       fetch('https://api.chucknorris.io/jokes/random?category=' + category)
           .then(res => res.json())
           .then((data) => {
-            this(alert(data.value))
+            this.setState({jokes: data })
           })
           .catch(console.log)
     }
