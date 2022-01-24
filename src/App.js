b3426163ae5a70f69f071adcb03b4cc3fd1cf94c
EvansVan <evansndiritu@gmail.com>
2022-01-25 00:58:40 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -24,34 +24,41 @@ import 'bootstrap/dist/css/bootstrap.css';
 
     render() {
       return (
-        <> 
-        {this.jaza()} 
-        {this.cards()} 
-        </>
-      );
-    }
+        <body>
+          <div class ="container mt-5" >
+            <div class="row pb-3">
+            {this.jaza()}  
+            {this.cards()}
+            </div>
+          </div>
+        </body>
+        );}
     jaza = (e) =>{ 
       let jokes = []
       let sasa = this.categories.length;
       for (let i=0; i<sasa;i++){
           jokes.push(  
-          <container fluid>
-            <button id={this.categories[i]} class="btn btn-primary btn-block" type="button" onClick={this.getChucked.bind(this)}>{this.categories[i]}</button>
-          </container>
+          <div class="col-3 pt-3">
+              <div class="w-100"></div>
+            <button id={this.categories[i]} class=" btn btn-primary btn-block" type="button" 
+                     onClick={this.getChucked.bind(this)}>{this.categories[i]}</button>
+          </div>
         )}
       return jokes
     }
     cards = (e) => {
       let  cadi = []
       cadi.push(
-        <div>
-          <center><h1>Jokes</h1></center>
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">{this.state.jokes.categories}</h5>
-                <p class="card-text">{this.state.jokes.value}</p>
+        <div class="mt-5">
+          <screenLeft><h1>Jokes</h1></screenLeft>
+          <div class="col-lg-3 col-md-6 col-sm-12  py-2 ">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">{this.state.jokes.categories}</h5>
+                  <p class="card-text">{this.state.jokes.value}</p>
+                </div>
               </div>
-            </div>
+          </div>  
         </div>
         )
     return cadi
